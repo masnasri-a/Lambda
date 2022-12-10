98a961b8d6a8acfa40ca34156b2a4f7f9ecf5db1
Nasri Adzlani <nasri@jkt1.ebdesk.com>
2022-12-10 14:49:00 +0700

    Push

--- functions/function.ts
+++ functions/function.ts
@@ -1,21 +1,23 @@
 import { Handler } from "aws-lambda";
-import { DynamoDBClient,ListTablesCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient,ListTablesCommand,GetItemCommand } from "@aws-sdk/client-dynamodb";
 
-const TABLE_NAME: string = "influencer";
+const TABLE_NAME: string = "IceTable";
 
 export const handler: Handler = async (event, context) => {
     // console.log(event);
     
 
-    
-    // const httpMethod = event.httpMethod;
-    // if (httpMethod == "GET") {
-    //     const item = await scan();
-    //     return {
-    //         statusCode: 200,
-    //         body: JSON.stringify(item), 
-    //     };
-    // }
+
+    const httpMethod = event.httpMethod;
+    if (httpMethod == "GET") {
+      const username = event.queryStringParameters.username;
+
+        const item = await getItem(username);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(item), 
+        };
+    }
     // else if (httpMethod == "POST") {
     //   return await save(event);
     // } else if (httpMethod == "DELETE") {
@@ -36,6 +38,37 @@ export const handler: Handler = async (event, context) => {
     // }
 };
 
+
+async function getItem(name : string ) {
+
+    const client = new DynamoDBClient({ region: "ap-southeast-1" });
+
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+          KEY_NAME: { N: name },
+        },
+      };
+
+      const data = await client.send(new GetItemCommand(params));
+  console.log("Success", data.Item);
+  return data;
+
+    // const params : DynamoDB.DocumentClient.GetItemInput = {
+    //     Key: {
+    //     username: name,
+    //   },
+    //   TableName: TABLE_NAME,
+    // };
+    
+    // return dynamo
+    //   .get(params)
+    //   .promise()
+    //   .then((result) => {
+    //     console.log(result);
+    //     return result.Item;
+    //   });
+  }
 
 async function scan() {
     console.log("SCAN");
