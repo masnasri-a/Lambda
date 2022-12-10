3a5ecdb1f755e9a3e8c3a25f337d7bac38749d0e
Nasri Adzlani <nasri@jkt1.ebdesk.com>
2022-12-10 12:58:12 +0700

    first commit

---
+++ functions/function.ts
@@ -0,0 +1,49 @@
+import { Handler } from "aws-lambda";
+import { DynamoDBClient,ListTablesCommand } from "@aws-sdk/client-dynamodb";
+
+const TABLE_NAME: string = "influencer";
+
+export const handler: Handler = async (event, context) => {
+    console.log(event);
+    const httpMethod = event.httpMethod;
+    if (httpMethod == "GET") {
+        const item = await scan();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(item),
+        };
+    }
+    // else if (httpMethod == "POST") {
+    //   return await save(event);
+    // } else if (httpMethod == "DELETE") {
+    //   const username = event.queryStringParameters.username;
+    //   if (username !== undefined) {
+    //     const item = await deletes(username);
+    //     return {
+    //       statusCode: 200,
+    //       body: JSON.stringify(item),
+    //     };
+    //   } else {
+    //     const message = "Nobody was greeted with that name";
+    //     return {
+    //       statusCode: 200,
+    //       body: JSON.stringify(message),
+    //     };
+    //   }
+    // }
+};
+
+
+async function scan() {
+    console.log("SCAN");
+    
+    const client = new DynamoDBClient({ region: "ap-southeast-1" });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+    return results
+  } catch (err) {
+    console.error(err);
+  }
+
+}
--- lib/icecdk-stack.ts
+++ lib/icecdk-stack.ts
@@ -1,10 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+// import * as lambda from '@aws-cdk/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IcecdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    
+    
+    new NodejsFunction(this, 'Influencer',{
+      functionName: "Influencer",
+      runtime: Runtime.NODEJS_14_X,
+      entry: path.join(__dirname, `/../functions/function.ts`),
+      handler: "handler",
+      timeout:cdk.Duration.seconds(300)
+  })
 
     // The code that defines your stack goes here
 
